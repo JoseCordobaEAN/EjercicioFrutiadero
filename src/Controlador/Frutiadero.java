5d05bbdf3cbc77e8d7d3c440503ca7f546313ab5
jmcordoba <jmcordoba@krustypc>
2017-11-09 18:50:38 -0500

    Maqueta de la solución del ejercicio

---
+++ src/Controlador/Frutiadero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ * Esta clase permite modelar el inventario del frutiadero asi como elaborar 
+ * ensaladas y jugos
+ * 
+ * @author jmcordoba
+ */
+public class Frutiadero {
+    
+}
---
+++ src/Modelo/Ensalada.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * Es uno de los productos de la compañía requiere frutas para su elaboración
+ * 
+ * @author jmcordoba
+ */
+public class Ensalada {
+    
+}
